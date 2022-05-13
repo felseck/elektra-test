251cccdd982ee4ed711d273253f278cb98f82590
Felipe Solorzano <felseck@hotmail.com>
2022-05-13 10:02:56 -0500

    start

---
+++ src/pages/page.categories.js
@@ -0,0 +1,111 @@
+import React, {useState,useEffect} from "react";
+import { Link } from "react-router-dom";
+import  axios  from "axios";
+
+const API_URL = "https://gila-software-test.herokuapp.com/api";
+
+function Categories() { 
+
+    const [categories, setCategories] = useState([]);
+    const [error, setError] = useState(null);
+
+
+
+
+      const getCategories = ()=>{
+      
+          axios.get(
+            `${API_URL}/categories`,{
+                params: {
+                }
+              })
+            .then(response => {
+              setCategories(response.data);
+            })
+            .catch(error => {
+              setError(error);
+            });
+        
+      }
+
+      useEffect(()=>{
+
+        getCategories();
+
+      },[])
+
+
+
+
+      const deleteCategory = ($id)=>{ 
+          if(!window.confirm('¿Desea eliminar esta categoria?')) return;
+
+          axios.delete(
+            `${API_URL}/categories/${$id}`,{
+                params: {
+                }
+              })
+            .then(response => {
+                getCategories();
+            })
+            .catch(error => {
+              setError(error);
+            });
+
+          
+      }
+
+      if(error){
+          alert(error);
+          setError(null);
+      }
+
+    return (
+         <div>
+             <h1>Categorias</h1>
+             
+             <Link to={`/category/create`} className="btn btn-success" >Agregar nueva categoria</Link>
+
+             
+             <div className="table-responsive">
+  
+             <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">% Utilidad</th>
+      <th scope="col">Acciones</th>
+    </tr>
+  </thead>
+  <tbody>
+
+  {categories.map((category,index)=>{
+    return (
+    <tr key={`${index}_${category.id}`}>
+      <th scope="row">{category.id}</th>
+      <td>{category.name}</td>
+      <td>{category.profit_margin_percent}%</td>
+      <td>
+
+      <div className="btn-group" role="group" aria-label="Acciones">
+      <Link to={`/category/update/${category.id}`} className="btn btn-primary" >Editar</Link>
+      <button type="button" className="btn btn-danger" onClick={(e)=> deleteCategory(category.id)}>Eliminar</button>
+      </div>
+
+      </td>
+    </tr>
+    )
+    })}
+    
+  </tbody>
+</table>
+
+</div>
+</div>
+    )
+
+}
+
+export default Categories;
+
---
+++ src/pages/page.product.details.js
@@ -0,0 +1,102 @@
+import React, {useState,useEffect} from "react";
+import AddToCart from "../components/component.cart.add";
+import  axios  from "axios";
+
+import {
+  useParams
+} from "react-router-dom";
+
+import { useSelector, useDispatch } from "react-redux";
+import {add} from "../actions/actions.cart.js";
+
+
+const API_URL = "https://gila-software-test.herokuapp.com/api";
+
+function ProductDetails(props) { 
+
+    let { id } = useParams();
+
+    const cart = useSelector((state) => state.cart);
+
+    const dispatch = useDispatch();
+
+    const [product, setProduct] = useState({});
+    const [error, setError] = useState(null);
+
+
+
+
+      const getProduct = ()=>{
+      
+          axios.get(
+            `${API_URL}/products/${id}`,{
+                params: {
+                 
+                }
+              })
+            .then(response => {
+              setProduct(response.data);
+            })
+            .catch(error => {
+              setError(error);
+            });
+        
+      }
+
+      useEffect(()=>{
+
+        getProduct();
+
+      },[])
+
+
+      if(error){
+        alert(error);
+        setError(null);
+    }
+
+
+    return (
+      <div>
+   <h1>Detalles</h1>
+   <div className="card">
+      <div className="card-body">
+         <h5 className="card-title">{product.name}</h5>
+         <h6 className="card-subtitle mb-2 text-muted">${product.price}</h6>
+         <p className="card-text">
+            Categoria:{product.category?product.category.name:''}
+         </p>
+         <div className="">
+         <h3>Especificaciones:</h3>
+         <ul>
+            {product.specs?product.specs.map((spec,index)=>{
+            return(
+            <li key={`specs_${index}`}>
+               {spec.spec.name}
+               <ul>
+                  {spec.attributes?spec.attributes.map((attribute,index_)=>{
+                  return(
+                  <li key={`attributes_${index_}`}>
+                     {attribute.attribute.name}
+                  </li>
+                  )
+                  }):''}
+               </ul>
+            </li>
+            )
+            }):''}
+         </ul>
+         </div>
+         
+         {/* add(product,3)  argumentos 1 = objeto del producto, argumento 2 = cantidad de productos */}
+         <button className="btn btn-success" onClick={() => dispatch(add(product,2))}>Agregar al carrito {cart.length}</button>
+           
+      </div>
+   </div>
+</div>
+    )
+
+}
+
+export default ProductDetails;
+
